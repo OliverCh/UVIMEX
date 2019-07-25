65d9f2a53d6cde6b1af6f8993438903b0fbe69bf
Benjamin <b.lopez@denlin.com.mx>
2019-07-25 17:03:45 -0500

    Creacion de archivos para actividades

---
+++ js/courseControllers/activityParser.js
@@ -0,0 +1,65 @@
+define(function(require){
+	var publics = {};
+
+	publics.parseType1 = function(data){
+		var html = "";
+		for (var i in data){
+			html += `
+				<div class="renglonAct">
+					<h2><span class="numberAct">`+i+`</span>`+data[i].questionString+`</h2>
+					<input class="activityAnswer" placeholder="Escribe tu respuesta...">
+				</div>`;
+		}
+		return html;
+	};
+	
+	publics.parseType2 = function(data){
+		var html = "";
+		for (var i in data){
+			var current = `
+				<div class="renglonAct">
+					<h2><span class="numberAct">`+i+`</span>`+data[i].questionString+`</h2>
+					:answers:
+				</div>`;
+			var answersHTML = "";
+			for (var j in data[i].answers){
+				answersHTML += `
+					<div class="multianswerdRow">
+						<div class="actContainer">
+							<input class="actInput" type="radio" value="`+data[j].answers[j].answerID+`">
+						</div>
+						<p>`+data[j].answers[j].answerStr+`</p>
+					</div>`;
+			}
+
+			html += current.replace(':answers:', answersHTML);
+		}
+		return html;
+	};
+
+	publics.parseType3 = function(data){
+		var html = "";
+		for (var i in data){
+			var current = `
+				<div class="renglonAct">
+					<h2><span class="numberAct">`+i+`</span>`+data[i].questionString+`</h2>
+					:answers:
+				</div>`;
+			var answersHTML = "";
+			for (var j in data[i].answers){
+				answersHTML += `
+					<div class="multianswerdRow">
+						<div class="actfb">
+							<p data-ansid="`+data[j].answers[j].answerID+`">`+data[j].answers[j].answerStr+`</p>
+						</div>
+						
+					</div>`;
+			}
+
+			html += current.replace(':answers:', answersHTML);
+		}
+		return html;
+	};
+
+	return publics;
+});
