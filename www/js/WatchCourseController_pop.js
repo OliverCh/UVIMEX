8f0bc9afcd893f47921101e2432ab557d39d7ed9
Benjamin <b.lopez@denlin.com.mx>
2019-07-26 20:31:22 -0500

    Agregados los estilos y el html (que esta en joto ese) para la barra de retroceso para cerrar el curso en ios. Y su funcionalidad

--- www/js/WatchCourseController_pop.js
+++ www/js/WatchCourseController_pop.js
@@ -20,7 +20,11 @@ define(function(require){
 		var moduleID = myData.moduleID;
 		var userID = myData.userID;
 		screen.orientation.lock('landscape-primary');
-		screenContainer.prepend(`<iframe src="https://uvimex.com.mx/dashboard/platform/php/preview.php?idmodule=${moduleID}&usr=${userID}"></iframe>`);
+		screenContainer.prepend(`
+			<div class="frameCourseViewer">
+				<div class="frameTop"><button class="closeCourse"><i class="fas fa-chevron-left"></i></button></div>
+				<iframe src="https://uvimex.com.mx/dashboard/platform/php/preview.php?idmodule=${moduleID}&usr=${userID}"></iframe>
+			</div>`);
 	}
 	
 	return publics;
