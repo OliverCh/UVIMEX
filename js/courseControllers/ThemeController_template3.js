642d0a37cda7e15b6ed48c1f17f4d87dd09833fd
Martin Rivas <m.rivas@denlin.com.mx>
2019-08-01 20:23:30 -0500

    + Agregada funcionalidad de obtencion de base de datos en template3 y 7

--- js/courseControllers/ThemeController_template3.js
+++ js/courseControllers/ThemeController_template3.js
@@ -5,6 +5,8 @@ define(function(require){
 
 	var audioController = null;
 	var themeAudio_ = null;
+	var theme = null;
+	var template = null;
 
 	publics.setContainer = function(cnt){
 		screenContainer = cnt;
@@ -15,17 +17,22 @@ define(function(require){
 		var base = $("<div></div>");
 		base.load("secciones/platform/audioSubcontroller.html", function(){
 			findFields();
-			$.post(masterPath + "getFullContent.php", {themeID: myData.theme.id, themeTemplate: myData.template}, function(data){
+			$.post(masterPath + "getFullContent.php", {themeID: theme.id, themeTemplate: template}, function(data){
 				console.log(data);
-				initVideo();
+				getFullContent();
 			});
 		});
 		base.appendTo(screenContainer);
 	}
 
 	publics.setData = function(data){
-		console.log('a', data);
-		myData = data;
+		if(data.theme !== undefined){
+			theme = data.theme;
+		}
+		if(data.template !== undefined){
+			template = data.template;
+		}
+
 		return this;
 	}
 
@@ -34,20 +41,61 @@ define(function(require){
 		return this;
 	}
 
-	var initVideo = function(){
+	var initVideo = function(audio, images, files){
 		require(["courseControllers/AudioSubcontroller"], function(AudioSubcontroller){
+			// audioController = AudioSubcontroller;
+			// audioController.setContainer(themeAudio_)
+			// 				.setParentNav(parentNav)
+			// 				.setAudioURL("https://sample-videos.com/audio/mp3/crowd-cheering.mp3")
+			// 				.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 0)
+			// 				.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 20)
+			// 				.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 10)
+			// 				.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/378/Files/1562872569d1c3d647e0fb.png")
+			// 				.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/372/Files/15627828930bc732e562be.pdf")
+			// 				.addFile("AAA", "https://docs.google.com/document/d/e/2PACX-1vRJVgVwOWeaNUf1ew6Ir8nHO18RqlEhz_Bd7Gsa1e58aj8hdvNs8tKfIZPocmF0cJae5JHM3-uvyqRo/pub")
+			// 				.addFile("Un dox", "https://uvimex.com.mx/dashboard/platform/uploads/Análisis-de-consumo-hídrico.docx")
+			// 				.draw();
+
 			audioController = AudioSubcontroller;
 			audioController.setContainer(themeAudio_)
 							.setParentNav(parentNav)
-							.setAudioURL("https://sample-videos.com/audio/mp3/crowd-cheering.mp3")
-							.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 0)
-							.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 20)
-							.addImageToShow("https://www.fujifilm.com/products/digital_cameras/x/fujifilm_x_pro2/sample_images/img/index/ff_x_pro2_003.JPG", 10)
-							.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/378/Files/1562872569d1c3d647e0fb.png")
-							.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/372/Files/15627828930bc732e562be.pdf")
-							.addFile("AAA", "https://docs.google.com/document/d/e/2PACX-1vRJVgVwOWeaNUf1ew6Ir8nHO18RqlEhz_Bd7Gsa1e58aj8hdvNs8tKfIZPocmF0cJae5JHM3-uvyqRo/pub")
-							.addFile("Un dox", "https://uvimex.com.mx/dashboard/platform/uploads/Análisis-de-consumo-hídrico.docx")
-							.draw();
+							.setAudioURL(audio);
+			for (var i = 0; i < images.length; i++) {
+				var image = images[i];
+				audioController = audioController.addImageToShow(image.url, image.time);
+				console.log(image);
+				console.log(image.url);
+				console.log(image.time);
+			}
+
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				audioController = audioController.addFile(file.name, file.url);
+			}
+			audioController.draw();
+		});
+	}
+
+	var getFullContent = function(){
+		$.ajax({
+			url: masterPath + "getFullContent.php",
+			data: {themeID: theme.id, themeTemplate: template},
+			success: function(data){
+				if(data.content === undefined || data.images === undefined || data.files === undefined){
+					throw new Error("Wrong data");
+					console.error(data);
+					return;
+				}
+				// convert from min:sec format
+				for (var i = data.images.length - 1; i >= 0; i--) {
+					var crudeTime = data.images[i].time;
+					var splited = crudeTime.split(":");
+					var min = parseInt(splited[0]);
+					var sec = parseInt(splited[1]);
+					data.images[i].time = min * 60 + sec; 
+				}
+				initVideo(data.content, data.images, data.files);
+			}
 		});
 	}
 
--- js/courseControllers/ThemeController_template7.js
+++ js/courseControllers/ThemeController_template7.js
@@ -5,6 +5,8 @@ define(function(require){
 
 	var videoController = null;
 	var themeText_ = null;
+	var theme = null;
+	var template = null;
 
 	publics.setContainer = function(cnt){
 		screenContainer = cnt;
@@ -15,12 +17,18 @@ define(function(require){
 		var base = $("<div></div>");
 		base.load("secciones/platform/textSubcontroller.html", function(){
 			findFields();
-			initText();
+			getFullContent();
 		});
 		base.appendTo(screenContainer);
 	}
 
 	publics.setData = function(data){
+		if(data.theme !== undefined){
+			theme = data.theme;
+		}
+		if(data.template !== undefined){
+			template = data.template;
+		}
 		myData = data;
 		return this;
 	}
@@ -30,22 +38,48 @@ define(function(require){
 		return this;
 	}
 
-	var initText = function(){
+	var initText = function(text, files){
 		require(["courseControllers/TextSubcontroller"], function(TextSubcontroller){
+			// textSubcontroller = TextSubcontroller;
+			// textSubcontroller.setContainer(themeText_)
+			// 				.setParentNav(parentNav)
+			// 				.setText(`<p><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Mujeres y activistas que defienden el derecho al aborto en&nbsp;</span><a href="https://cnnespanol.cnn.com/seccion/estados-unidos/" title="Estados Unidos" target="_blank" class="auto-link" style="color: rgb(203, 0, 0); font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Estados Unidos</a><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">&nbsp;se manifestaron para tratar de impedir que avancen las iniciativas como las recientemente aprobadas en Georgia y Alabama. Gustavo Valdés con el reporte.</span></p><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;"><br></span><p><span style="color: rgb(38, 38, 38); font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Los demócratas de la Cámara de Representantes se amontonaron el miércoles cuando la portavoz Nancy Pelosi intentó reprimir una creciente revuelta por su resistencia a la idea de destituir al presidente Donald Trump. La reunión se produjo después de días de mayor voluntad pública por parte de líderes demócratas de alto rango para sugerir que la destitución es el movimiento correcto y que se ha convertido en una cuestión de cuándo se hará y no de si debe hacerse.</span></p>`)
+			// 				.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/378/Files/1562872569d1c3d647e0fb.png")
+			// 				.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/372/Files/15627828930bc732e562be.pdf")
+			// 				.addFile("AAA", "https://docs.google.com/document/d/e/2PACX-1vRJVgVwOWeaNUf1ew6Ir8nHO18RqlEhz_Bd7Gsa1e58aj8hdvNs8tKfIZPocmF0cJae5JHM3-uvyqRo/pub")
+			// 				.addFile("Un dox", "https://uvimex.com.mx/dashboard/platform/uploads/Análisis-de-consumo-hídrico.docx")
+			// 				.draw();
+			
 			textSubcontroller = TextSubcontroller;
 			textSubcontroller.setContainer(themeText_)
-							.setParentNav(parentNav)
-							.setText(`<p><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Mujeres y activistas que defienden el derecho al aborto en&nbsp;</span><a href="https://cnnespanol.cnn.com/seccion/estados-unidos/" title="Estados Unidos" target="_blank" class="auto-link" style="color: rgb(203, 0, 0); font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Estados Unidos</a><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">&nbsp;se manifestaron para tratar de impedir que avancen las iniciativas como las recientemente aprobadas en Georgia y Alabama. Gustavo Valdés con el reporte.</span></p><span style="font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;"><br></span><p><span style="color: rgb(38, 38, 38); font-family: CNNSans-Light, &quot;Helvetica Neue&quot;, Arial; font-size: 18px;">Los demócratas de la Cámara de Representantes se amontonaron el miércoles cuando la portavoz Nancy Pelosi intentó reprimir una creciente revuelta por su resistencia a la idea de destituir al presidente Donald Trump. La reunión se produjo después de días de mayor voluntad pública por parte de líderes demócratas de alto rango para sugerir que la destitución es el movimiento correcto y que se ha convertido en una cuestión de cuándo se hará y no de si debe hacerse.</span></p>`)
-							.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/378/Files/1562872569d1c3d647e0fb.png")
-							.addFile("pronombres", "https://uvimex.com.mx/dashboard/platform/uploads/372/Files/15627828930bc732e562be.pdf")
-							.addFile("AAA", "https://docs.google.com/document/d/e/2PACX-1vRJVgVwOWeaNUf1ew6Ir8nHO18RqlEhz_Bd7Gsa1e58aj8hdvNs8tKfIZPocmF0cJae5JHM3-uvyqRo/pub")
-							.addFile("Un dox", "https://uvimex.com.mx/dashboard/platform/uploads/Análisis-de-consumo-hídrico.docx")
-							.draw();
+								.setParentNav(parentNav)
+								.setText(text);
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				textSubcontroller = textSubcontroller.addFile(file.name, file.url);
+			}
+
+			textSubcontroller.draw();
 		});
 	}
 
 	var findFields = function(){
 		themeText_ = screenContainer.find("#themeText_");
+	}
+
+	var getFullContent = function(){
+		$.ajax({
+			url: masterPath + "getFullContent.php",
+			data: {themeID: theme.id, themeTemplate: template},
+			success: function(data){
+				if(data.content === undefined || data.files === undefined){
+					throw new Error("Wrong data");
+					console.error(data);
+					return;
+				}
+				initText(data.content, data.files);
+			}
+		});
 	}
 	return publics;
 });
